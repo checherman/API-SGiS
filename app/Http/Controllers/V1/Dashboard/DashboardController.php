a6ba9ff03738ab54b7466e9dda04d73a82c9a284
Luis Alberto Valdez Lescieur <Luisvl13@gmail.com>
2017-12-20 11:17:05 -0600

    Correccione en Dashboard en triage

--- app/Http/Controllers/V1/Dashboard/DashboardController.php
+++ app/Http/Controllers/V1/Dashboard/DashboardController.php
@@ -11,19 +11,27 @@ class DashboardController
     public function index()
     {
         $datos = array();
+        $cluesH = Request::header('clues');
+
 
         //Triage 1.-Verde 2.-Amarillo 3.-Rojo
         $totalVerde = Incidencias::select('incidencias.*')->with("pacientes.personas", "pacientes.acompaniantes.personas")
             ->with("movimientos_incidencias", "referencias", "altas_incidencias", "estados_incidencias")->join('movimientos_incidencias', 'movimientos_incidencias.incidencias_id', '=', 'incidencias.id')
-            ->where('movimientos_incidencias.triage_colores_id', 1)->count();
+            ->where('movimientos_incidencias.triage_colores_id', 1)
+            ->join('incidencia_clue', 'incidencia_clue.incidencias_id', '=', 'incidencias.id')
+            ->where('incidencia_clue.clues',$cluesH)->count();
 
         $totalAmarillo = Incidencias::select('incidencias.*')->with("pacientes.personas", "pacientes.acompaniantes.personas")
             ->with("movimientos_incidencias", "referencias", "altas_incidencias", "estados_incidencias")->join('movimientos_incidencias', 'movimientos_incidencias.incidencias_id', '=', 'incidencias.id')
-            ->where('movimientos_incidencias.triage_colores_id', 2)->count();
+            ->where('movimientos_incidencias.triage_colores_id', 2)
+            ->join('incidencia_clue', 'incidencia_clue.incidencias_id', '=', 'incidencias.id')
+            ->where('incidencia_clue.clues',$cluesH)->count();
 
         $totalRojo = Incidencias::select('incidencias.*')->with("pacientes.personas", "pacientes.acompaniantes.personas")
             ->with("movimientos_incidencias", "referencias", "altas_incidencias", "estados_incidencias")->join('movimientos_incidencias', 'movimientos_incidencias.incidencias_id', '=', 'incidencias.id')
-            ->where('movimientos_incidencias.triage_colores_id', 3)->count();
+            ->where('movimientos_incidencias.triage_colores_id', 3)
+            ->join('incidencia_clue', 'incidencia_clue.incidencias_id', '=', 'incidencias.id')
+            ->where('incidencia_clue.clues',$cluesH)->count();
 
         $triage = array();
         array_push($triage, ['nombre' => 'Verde', 'total' => $totalVerde]);
